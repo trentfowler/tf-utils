d6f09dc2b0991b1e860db50f4e8c0952f6534ac5
Trent Fowler <trenttfowler@gmail.com>
2019-10-27 02:39:08 -0500

    Add archivedir script to create compressed archive of dir

---
+++ bin/archivedir.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#archivedir--Creates a compressed archive of the specified directory.
+
+# Size of a big directory in blocks.
+maxarchivedir=10
+compress=gzip
+# Nicer format for error messages.
+progname=$(basename $0)
+# Arguments required.
+if [ $# -eq 0 ]; then
+  echo "usage: $progname directory" >&2
+  exit 1
+fi
+if [ ! -d "$1" ]; then
+  echo "${progname}: cannot find directory $1 to archive." >&2
+  exit 1
+fi
+if [ "$(basename "$1")" != "$1" -o "$1" = "." ]; then
+  echo "${progname}: You must specify a subdirectory." >&2
+  exit 1
+fi
+if [ ! -w . ]; then
+  echo "${progname}: cannot write archive file to current directory." >&2
+  exit 1
+fi
+# Check to see if the resultant archive is going to be dangerously big.
+dirsize="$(du -s "$1" | awk '{print $1}')"
+if [ $dirsize -gt $maxarchivedir ]; then
+  /bin/echo -n "warning: directory $1 is $dirsize blocks. Proceed? [n] "
+  read answer
+  answer="$(echo $answer | tr '[:upper:]' '[:lower:]' | cut -c1)"
+  if [ "$answer" != "y" ]; then
+    echo "${progname}: archive of directory $1 canceled." >&2
+    exit 0
+  fi
+fi
+archivename="$1.tgz"
+if tar cf - $1 | $compress > $archivename ; then
+  echo "Directory $1 archived as $archivename"
+else
+  echo "warning: tar encountered errors archiving $1"
+fi
+exit 0
